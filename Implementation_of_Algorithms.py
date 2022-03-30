c39abcc2086f8bb390c3754c4d1c2a5f1432829b
Bill NIkolo <vasileios.nikolopoulos.001@student.uni.lu>
2022-03-30 13:27:32 +0200

    dfs implementation

--- Implementation_of_Algorithms.py
+++ Implementation_of_Algorithms.py
@@ -1,18 +1,31 @@
 star_galactica = { "Ajan Kloss": {"Bahu": 15, "Bothawui": 11, "Bespin and Hoth": 20},
                    "Bahu": {"Ach-To" : 6, "Endor and Kef Bir": 4},
-                   "Bothawui": {"Cantanice": 12, "Kessel": 5},
+                   "Bothawui": {"Cantanica": 12, "Kessel": 5},
                    "Bespin and Hoth": {"Crait": 5, "D'Qar": 6},
                    "Ach-To": {"Cantanica": 22},
                    "Endor and Kef Bir": {"Sullust": 10, "Ring of Kafrene": 5},
-                   "D'Qar": {"Sullust": 2, "Naboo": 2}}
+                   "D'Qar": {"Sullust": 2, "Naboo": 2},
+                   "Cantanica": {},
+                   "Sullust": {},
+                   "Ring of Kafrene": {},
+                   "Kessel": {},
+                   "Crait": {},
+                   "Naboo": {}
+                   }
 
-
-def dfs(visited, graph, node):
-    visited = set() # Set to keep track of visited nodes.
+# target = ["Sullust", "Cantanica"]
+def dfs(visited,graph, node, target):
+    visited = set()
+    if node in target:
+      print(target, "Found")
+      quit()
     if node not in visited:
-        print ("dfs",node)
-        if node == target:
-            print("Found:",node)
+        print ("->", node)
         visited.add(node)
         for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
+            dfs(visited, graph, neighbour, target)
+
+
+# dfs([], star_galactica, "Ajan Kloss", "Naboo")
+
+
