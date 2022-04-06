3672522d92107c48cbef166cca36b86633f4e120
Peral175 <perrardalex@yahoo.com>
2022-04-06 13:36:26 +0200

    code update

--- Implementation_of_Algorithms.py
+++ Implementation_of_Algorithms.py
@@ -1,10 +1,15 @@
-star_galactica = { "Ajan Kloss": {"Bahu": 15, "Bothawui": 11, "Bespin and Hoth": 20},
-                   "Bahu": {"Ach-To" : 6, "Endor and Kef Bir": 4},
-                   "Bothawui": {"Cantanica": 12, "Kessel": 5},
-                   "Bespin and Hoth": {"Crait": 5, "D'Qar": 6},
-                   "Ach-To": {"Cantanica": 22},
-                   "Endor and Kef Bir": {"Sullust": 10, "Ring of Kafrene": 5},
-                   "D'Qar": {"Sullust": 2, "Naboo": 2},
+'''
+Python dictionary containing 1 dictionary for each node.
+The internal dictionary contains the children of the outer node, as well as the associated weights. 
+Nodes: edge={child node: distance} 
+'''
+star_galactica = { "Ajan Kloss": {"Bahu": 23, "Bothawui": 17, "Bespin and Hoth": 30},
+                   "Bahu": {"Ach-To" : 9, "Endor and Kef Bir": 6},
+                   "Bothawui": {"Cantanica": 18, "Kessel": 8},
+                   "Bespin and Hoth": {"Crait": 8, "D'Qar": 9},
+                   "Ach-To": {"Cantanica": 33},
+                   "Endor and Kef Bir": {"Sullust": 15, "Ring of Kafrene": 8},
+                   "D'Qar": {"Sullust": 3, "Naboo": 3},
                    "Cantanica": {},
                    "Sullust": {},
                    "Ring of Kafrene": {},
@@ -12,7 +17,6 @@ star_galactica = { "Ajan Kloss": {"Bahu": 15, "Bothawui": 11, "Bespin and Hoth":
                    "Crait": {},
                    "Naboo": {}
                    }
-
 
 def dfs(visited, graph, origin, target, found):
     global temp_found
@@ -29,40 +33,29 @@ def dfs(visited, graph, origin, target, found):
         for neighbour in graph[origin]:
             dfs(visited, graph, neighbour, target, temp_found)
 
-# def ucs(graph, origin, target):
-#   list_of_next_node = [(0, origin)]
-#   visited.append(origin)
-#   if origin == target:
-#     quit()
-#   counter = 1
-#   while True:
-#     counter += 1
-#     list_of_next_node = sorted(list_of_next_node)
-#     s = list_of_next_node[0]
-#     list_of_next_node.pop(0)
-#     print(graph[s[1]])
-#     for neighbour in graph[s[1]]:
-#       list_of_next_node.append((graph[s[1]][neighbour], neighbour))
-#     s = sorted(list_of_next_node)
-#     print(s)
-#     if counter == 4:
-#       break
-
-# ucs(star_galactica, 'Ajan Kloss', "whatever")
-# def bfs(visited, graph, node):
-#     print(visualize_tree())
-#     visited.append(node)
-#     queue.append(node)
-#     while queue:
-#         s = queue.pop(0)
-#         print("bfs",s)
-#         if s == target:
-#             print("Found: ",s)
-#             break
-#         for neighbour in graph[s]:
-#             if neighbour not in visited:
-#                 visited.append(neighbour)
-#                 queue.append(neighbour)
+def ucs(graph, origin, target):
+  visited = []
+  # copy = graph.copy()
+  previous = None
+  list_of_next_node = [(0, origin)]
+  visited.append(origin)
+  if origin == target:
+    print("found",origin)
+    quit()
+  res = None
+  while res != target:
+    list_of_next_node = sorted(list_of_next_node)
+    s = list_of_next_node[0]
+    list_of_next_node.pop(0)
+    print("s:",s,graph[s[1]])
+    previous = s[0]
+    for neighbour in graph[s[1]]:
+      list_of_next_node.append((graph[s[1]][neighbour]+previous, neighbour))
+    s = sorted(list_of_next_node)
+    print("S:",s,target)
+    res = s[0][1]
+    print("Res: ",res)
+  print("found",res)
 
 def best_path_calculator(graph, src):
   queue = [src]
@@ -71,6 +64,7 @@ def best_path_calculator(graph, src):
   predecessor = {}
 
   while queue:
+    print("A",minDistances, predecessor)
     currentNode = queue.pop(0)
     for neighbor in graph[currentNode]:
       newDist = minDistances[currentNode] + graph[currentNode][neighbor]
@@ -85,6 +79,8 @@ def uniform_cost_search(graph, origin, dest):
   minDistances, predecessor = best_path_calculator(graph, origin)
   path = []
   currentNode = dest
+  print("B",minDistances, predecessor)
+  print("Current node: ", currentNode)
   while currentNode != origin:
     if currentNode not in predecessor:
       print("Path not reachable")
@@ -92,6 +88,7 @@ def uniform_cost_search(graph, origin, dest):
     else:
       path.insert(0, currentNode)
       currentNode = predecessor[currentNode]
+      print("Curr",currentNode)
   path.insert(0, origin)
 
   if dest in minDistances and minDistances[dest] != float("inf"):
@@ -100,9 +97,11 @@ def uniform_cost_search(graph, origin, dest):
   else:
     print("No possible path to that planet")
 
-# ///////////////////////////////////////////////// Execution Area ///////////////////////////////////////////////
+def main():
+  '''///////////////////////////////////////// Execution Area ////////////////////////////////////////////'''
+  dfs(set(), star_galactica, 'Ajan Kloss', "Sullust", False)
+  uniform_cost_search(star_galactica, 'Ajan Kloss', "Sullust")
+  ucs(star_galactica, 'Ajan Kloss', "Sullust") # path for ucs | (sort not by name) | no possible path? || rewrite 
 
-found = False
-visited = set()
-dfs(visited, star_galactica, 'Ajan Kloss', "Sullust", False)
-uniform_cost_search(star_galactica, 'Ajan Kloss', "Sullust")
+if __name__ == '__main__':
+  main()
